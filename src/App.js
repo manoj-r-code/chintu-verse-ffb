e549760f02ba1ce0fe91c0b406c30c9a3975b00c
manoj-r-code <manu.ravindra.123@gmail.com>
2025-04-11 19:47:03 +0530

    Revert "designed nav-bar"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import "./App.css";
-import NavBar from "./components/NavBar";
-import ThemeToggle from "./components/ThemeToggle";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <NavBar />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
